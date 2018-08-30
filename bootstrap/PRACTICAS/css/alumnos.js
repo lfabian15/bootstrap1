895427f58b10e740b034482ad55219dd382c9f4c
lfabian15 <42388184+lfabian15@users.noreply.github.com>
2018-08-30 00:17:44 -0500

    Add files via upload

---
+++ bootstrap/PRACTICAS/css/alumnos.js
@@ -0,0 +1,109 @@
+alumnos = [
+    {
+        "nombre" : Diego,
+        "apellido" : "Cueto",
+        "edad" : 23,
+        "sexo" : "H",
+        "gmail" : "diegolievano@hotmail.com",
+        "telefono" : 9661234567,
+        "direccioncalle" : "Veinte de noviembre",
+        "colonia" : "La providencia",
+        "municipio" : "Tonala",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Lucio",
+        "apellido" : "Escobar",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "lucio@hotmail.com",
+        "telefono" : 9681197536,
+        "direccioncalle" : "Revolucion",
+        "colonia" : "La provincia",
+        "municipio" : "Jiquipilas",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Tomas",
+        "apellido" : "Lopez",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "talm@hotmail.com",
+        "telefono" : 9684334567,
+        "direccioncalle" : "Nueva Palestina",
+        "colonia" : "La providencia",
+        "municipio" : "Cintalap",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Roxana",
+        "apellido" : "Machado",
+        "edad" : 23,
+        "sexo" : "M",
+        "gmail" : "rox@hotmail.com",
+        "telefono" : 9689754567,
+        "direccioncalle" : "La novena",
+        "colonia" : "Urbana",
+        "municipio" : "Cintalapa",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Fidel",
+        "apellido" : "Castro",
+        "edad" : 22,
+        "sexo" : "H",
+        "gmail" : "vegeta@hotmail.com",
+        "telefono" : 9689984521,
+        "direccioncalle" : "Las Rosas",
+        "colonia" : "Tepe",
+        "municipio" : "Cintalapa",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Lorena",
+        "apellido" : "Martinez",
+        "edad" : 22,
+        "sexo" : "M",
+        "gmail" : "lore@hotmail.com",
+        "telefono" : 9689875671,
+        "direccioncalle" : "Callejon de la amistad",
+        "colonia" : "Plan de ayala",
+        "municipio" : "Cintalapa",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    },
+    
+    {
+        "nombre" : "Guadalupe",
+        "apellido" : "Indili",
+        "edad" : 22,
+        "sexo" : "M",
+        "gmail" : "gua@hotmail.com",
+        "telefono" : 9661234568,
+        "direccioncalle" : "Aceradero",
+        "colonia" : "La Guaracha",
+        "municipio" : "Pijijiapan",
+        "estado" : "Chiapas",
+        "pais" : "Mexico"
+        
+    }
+    
+    
+    
+];
